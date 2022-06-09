46849f1acbdbe721b980e4c784fc8782e8fb5cd3
shawn.mathews <shawn.mathews@stmartin.edu>
2022-06-09 14:39:55 -0700

    exercise 28

---
+++ subq-vs-joins/query.sql
@@ -0,0 +1,9 @@
+SELECT DISTINCT 
+	customer_id,
+    first_name,
+    last_name
+FROM customers c
+JOIN orders o 
+	USING (customer_id)
+JOIN order_items oi USING (order_id)
+WHERE oi.product_id = 3
